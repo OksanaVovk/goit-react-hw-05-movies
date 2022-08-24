1864a7b84f697f44e449dd8e00c1754275f01c52
OksanaVovk <oksvovk85@gmail.com>
2022-08-24 23:36:44 +0300

    change6

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { lazy, Suspense } from 'react';
-import { useState, useEffect } from 'react';
-import fetchFilm from 'fetchFilms';
+// import { useState, useEffect } from 'react';
+// import fetchFilm from 'fetchFilms';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import { Link } from './App-styled';
 
@@ -11,26 +11,26 @@ const Cast = lazy(() => import('./Cast'));
 const Reviews = lazy(() => import('./Reviews'));
 
 export const App = () => {
-  const [searchWord, setSearchWord] = useState('');
-  const [moviesSearch, setMoviesSearch] = useState([]);
+  // const [searchWord, setSearchWord] = useState('');
+  // const [moviesSearch, setMoviesSearch] = useState([]);
 
-  const handleFormSubmit = searchWord => {
-    setSearchWord(searchWord);
-  };
+  // const handleFormSubmit = searchWord => {
+  //   setSearchWord(searchWord);
+  // };
 
-  useEffect(() => {
-    if (searchWord === '') {
-      return;
-    } else {
-      const KEY_API = '024bf82d4805f650033dc69997860333';
-      const url = `https://api.themoviedb.org/3/search/movie?api_key=${KEY_API}&language=en-US&page=1&include_adult=false&query=${searchWord}`;
-      fetchFilm(url)
-        .then(movie => {
-          setMoviesSearch(movie.results);
-        })
-        .catch(error => console.log(error));
-    }
-  }, [searchWord]);
+  // useEffect(() => {
+  //   if (searchWord === '') {
+  //     return;
+  //   } else {
+  //     const KEY_API = '024bf82d4805f650033dc69997860333';
+  //     const url = `https://api.themoviedb.org/3/search/movie?api_key=${KEY_API}&language=en-US&page=1&include_adult=false&query=${searchWord}`;
+  //     fetchFilm(url)
+  //       .then(movie => {
+  //         setMoviesSearch(movie.results);
+  //       })
+  //       .catch(error => console.log(error));
+  //   }
+  // }, [searchWord]);
 
   return (
     <div>
@@ -41,12 +41,7 @@ export const App = () => {
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route
-            path="/movies"
-            element={
-              <Movies onSubmit={handleFormSubmit} moviesArrey={moviesSearch} />
-            }
-          />
+          <Route path="/movies" element={<Movies />} />
           <Route path="/movies/:movieId" element={<MovieDetail />}>
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,28 +1,62 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import fetchFilm from 'fetchFilms';
 import { useSearchParams } from 'react-router-dom';
 import MovieList from 'components/MovieList';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const Movies = ({ onSubmit, moviesArrey }) => {
+const Movies = () => {
   const [inputValue, setInputValue] = useState('');
+  const [searchWord, setSearchWord] = useState('');
+  const [moviesSearch, setMoviesSearch] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
   const query = searchParams.get('query');
 
   const handleSearchChange = event => {
     setInputValue(event.currentTarget.value.toLowerCase());
-    setSearchParams({ query: event.currentTarget.value });
   };
+
+  //   const handleSubmit = event => {
+  //     event.preventDefault();
+  //     if (inputValue === '') {
+  //       return Notify.info('Please enter search data.');
+  //     } else if (query) {
+  //       onSubmit(query);
+  //     } else {
+  //       onSubmit(inputValue);
+  //     }
+  //     };
 
   const handleSubmit = event => {
     event.preventDefault();
     if (inputValue === '') {
       return Notify.info('Please enter search data.');
-    } else if (query) {
-      onSubmit(query);
     } else {
-      onSubmit(inputValue);
+      setSearchWord(inputValue);
+      setSearchParams({ query: inputValue });
     }
   };
+
+  useEffect(() => {
+    if (query) {
+      setSearchWord(query);
+    }
+  }, [query]);
+
+  useEffect(() => {
+    if (searchWord === '') {
+      return;
+    } else {
+      const KEY_API = '024bf82d4805f650033dc69997860333';
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=${KEY_API}&language=en-US&page=1&include_adult=false&query=${searchWord}`;
+      fetchFilm(url)
+        .then(movie => {
+          setMoviesSearch(movie.results);
+          setSearchWord('');
+          setInputValue('');
+        })
+        .catch(error => console.log(error));
+    }
+  }, [searchWord]);
 
   return (
     <main>
@@ -30,12 +64,12 @@ const Movies = ({ onSubmit, moviesArrey }) => {
         <input
           type="text"
           name="query"
-          value={query}
+          value={inputValue}
           onChange={handleSearchChange}
         ></input>
         <button type="submit">Search</button>
       </form>
-      <MovieList moviesAr={moviesArrey}></MovieList>
+      <MovieList moviesAr={moviesSearch}></MovieList>
     </main>
   );
 };
